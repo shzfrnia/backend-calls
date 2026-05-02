906048793d6ea9a7b8428f09f8c6c4d2338587e3
Mikhail Uljakov <mikhail.ulyakov@icloud.com>
2026-05-02 14:11:55 +0700

    CALL-68 WIP

--- backend/app/api/deps.py
+++ backend/app/api/deps.py
@@ -43,11 +43,14 @@ def get_current_user(session: SessionDep, token: TokenDep) -> User:
             status_code=status.HTTP_403_FORBIDDEN,
             detail="Could not validate credentials",
         )
+
     user = session.get(User, token_data.sub)
     if not user:
-        raise HTTPException(status_code=404, detail="User not found")
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
     if not user.is_active:
-        raise HTTPException(status_code=400, detail="Inactive user")
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="Inactive user"
+        )
     return user
 
 
